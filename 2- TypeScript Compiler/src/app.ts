c26259d5262a951dbd55bf0176d66eedbd003127
Majdi Mokhtar <majdi.mokhtar@gmail.com>
2022-12-20 17:01:52 +0100

    tsconfig

---
+++ 2- TypeScript Compiler/src/app.ts
@@ -0,0 +1,13 @@
+const userName = 'Majdi';
+
+console.log(userName);
+
+const btn = document.querySelector(".btn")
+
+if (btn) {
+    // let namee = "majdi"
+    btn.addEventListener("click",()=>{
+        console.log("clicked");
+        })
+}
+
