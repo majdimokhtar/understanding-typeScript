69c088fc322d4719d9a004db3d8b213b2d7ee454
Majdi Mokhtar <majdi.mokhtar@gmail.com>
2022-12-21 15:26:56 +0100

    continue working with classes

--- 4- Classes/src/app.ts
+++ 4- Classes/src/app.ts
@@ -1,14 +1,16 @@
 // Code goes here!
 
-class Departmen {
-  name: string
-  private employees: string[] = []
+class Department {
+  // private id: string
+  // private name: string
+  protected employees: string[] = []
 
-  constructor(n: string) {
-    this.name = n
+  constructor(private readonly id: string, public name: string) {
+    // this.id = id
+    // this.name = n
   }
-  describe(this: Departmen) {
-    console.log("department :" + this.name)
+  describe(this: Department) {
+    console.log(`Department (${this.id}):(${this.name})`)
   }
 
   addEmployee(employee: string) {
@@ -20,13 +22,65 @@ class Departmen {
   }
 }
 
-const accounting = new Departmen("Accounting")
+class ITDepartment extends Department {
+  admins: string[]
+  constructor(id: string, admins: string[]) {
+    super(id, "Accounting")
+    this.admins = admins
+  }
+}
+class AccountingDepartment extends Department {
+  private lastReport: string
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport
+    }
+    throw new Error("no report found")
+  }
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("please add a valid value")
+    }
+    this.addReports(value)
+  }
+  constructor(id: string, private reports: string[]) {
+    super(id, "IT")
+    this.lastReport = this.reports[0]
+  }
+  addReports(text: string) {
+    this.reports.push(text)
+    this.lastReport = text
+  }
+  addEmployee(name: string) {
+    if (name === "Majdi") {
+      return
+    }
+    this.employees.push(name)
+  }
+  printReports() {
+    console.log(this.reports)
+  }
+  printEmployee() {
+    console.log(this.employees)
+  }
+}
 
+const it = new ITDepartment("d1", ["Majdi"])
+
+it.addEmployee("Majdi")
+it.addEmployee("Mokhtar")
+it.describe()
+it.printEmployeeInfo()
+
+console.log(it)
+
+const accounting = new AccountingDepartment("d2", [])
+accounting.mostRecentReport = "sssss"
+accounting.addReports("something")
+accounting.printReports()
 accounting.addEmployee("Majdi")
 accounting.addEmployee("Mokhtar")
-accounting.describe()
-accounting.printEmployeeInfo()
+accounting.printEmployee()
+console.log(accounting.mostRecentReport)
 
-console.log(accounting)
-
-accounting.describe()
+// it.describe()
