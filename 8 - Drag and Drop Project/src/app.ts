af3f48e51867ab8ab708c3441aa97680de475081
Majdi Mokhtar <majdi.mokhtar@gmail.com>
2022-12-25 19:01:34 +0100

    form render

--- 8 - Drag and Drop Project/src/app.ts
+++ 8 - Drag and Drop Project/src/app.ts
@@ -1,1 +1,20 @@
-// Code goes here!
+class ProjectInput {
+  templetaElement: HTMLTemplateElement
+  hotsElement: HTMLDivElement
+  element: HTMLFormElement
+  constructor() {
+    this.templetaElement = document.querySelector(
+      "#project-input"
+    )! as HTMLTemplateElement
+    this.hotsElement = document.querySelector("#app")! as HTMLDivElement
+    const importedNode = document.importNode(this.templetaElement.content, true)
+    this.element = importedNode.firstElementChild as HTMLFormElement
+    this.attach()
+  }
+  private attach() {
+    this.hotsElement.insertAdjacentElement("afterbegin", this.element)
+  }
+}
+
+
+const projectInput =  new ProjectInput()
