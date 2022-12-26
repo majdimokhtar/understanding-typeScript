ce1b2425849e35b9238b1d099c10dcbe6aba128a
Majdi Mokhtar <majdi.mokhtar@gmail.com>
2022-12-26 19:51:36 +0100

    redring list items

--- 8 - Drag and Drop Project/src/app.ts
+++ 8 - Drag and Drop Project/src/app.ts
@@ -1,3 +1,35 @@
+// project state management
+class ProjectState {
+  private listners: any[] = []
+  private projects: any[] = []
+  private static instance: ProjectState
+  private constructor() {}
+  static getInstance() {
+    if (this.instance) {
+      return this.instance
+    }
+    this.instance = new ProjectState()
+    return this.instance
+  }
+  addListner(listnerFn: Function) {
+    this.listners.push(listnerFn)
+  }
+  addProject(title: string, description: string, numOfPeople: number) {
+    const newProject = {
+      id: Math.random().toString(),
+      title: title,
+      description: description,
+      people: numOfPeople,
+    }
+    this.projects.push(newProject)
+    for (const listnerFn of this.listners) {
+      listnerFn(this.projects.slice())
+    }
+  }
+}
+
+const projectState = ProjectState.getInstance()
+
 //validation
 interface Validatble {
   value: string | number
@@ -56,6 +88,52 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   return adjDescriptor
 }
 
+// project list class
+
+class ProjectList {
+  templetaElement: HTMLTemplateElement
+  hotsElement: HTMLDivElement
+  element: HTMLElement
+  assignedProjects: any[]
+  constructor(private type: "active" | "finished") {
+    this.templetaElement = document.querySelector(
+      "#project-list"
+    )! as HTMLTemplateElement
+    this.hotsElement = document.querySelector("#app")! as HTMLDivElement
+    this.assignedProjects =[]
+    const importedNode = document.importNode(this.templetaElement.content, true)
+    this.element = importedNode.firstElementChild as HTMLElement
+    this.element.id = `${this.type}-projects`
+    projectState.addListner((projects: any[]) => {
+      this.assignedProjects = projects
+      this.renderProjects()
+    })
+    this.attach()
+    this.renderContent()
+  }
+
+  addProject() {}
+  private renderProjects() {
+    const listEl = document.getElementById(
+      `${this.type}-projects-list`
+    )! as HTMLUListElement
+    for (const projItem of this.assignedProjects) {
+      const listItem = document.createElement("li")
+      listItem.textContent = projItem.title
+      listEl.appendChild(listItem)
+    }
+  }
+  private renderContent() {
+    const listId = `${this.type}-projects-list`
+    this.element.querySelector("ul")!.id = listId
+    this.element.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + " PROJECTS"
+  }
+  private attach() {
+    this.hotsElement.insertAdjacentElement("beforeend", this.element)
+  }
+}
+
 // project input class
 class ProjectInput {
   templetaElement: HTMLTemplateElement
@@ -97,7 +175,7 @@ class ProjectInput {
     const descriptionValidatble: Validatble = {
       value: entredDesc,
       required: true,
-      minLength : 5
+      minLength: 5,
     }
     const peopleValidatble: Validatble = {
       value: +entredPeople,
@@ -124,7 +202,8 @@ class ProjectInput {
     console.log(this.titleInputElement.value)
     if (Array.isArray(userInput)) {
       const [title, description, people] = userInput
-      console.log(title, description, people)
+      projectState.addProject(title, description, people)
+      // console.log(title, description, people)
       this.clearInput()
     }
   }
@@ -142,3 +221,5 @@ class ProjectInput {
 }
 
 const projectInput = new ProjectInput()
+const activeProjectList = new ProjectList("active")
+const finishedProjectList = new ProjectList("finished")
