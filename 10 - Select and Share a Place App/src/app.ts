e3b627a13910a258a7d5fd9887efa995b4881f45
Majdi Mokhtar <majdi.mokhtar@gmail.com>
2022-12-29 23:10:49 +0100

    fetching geolocation with axios

--- 10 - Select and Share a Place App/src/app.ts
+++ 10 - Select and Share a Place App/src/app.ts
@@ -1,9 +1,28 @@
+import axios from "axios"
 const form = document.querySelector("form")!
 const addressInput = document.querySelector("#address")! as HTMLInputElement
 
-function searchAddressHandler (event:Event){
-event.preventDefault()
-const entredAddress =addressInput.nodeValue
+const GOOGLE_API_KEY = "AIzaSyCIaAc2c5M3VpbCH6PPq_guwy9lHuowXOs"
+type GoogleGeoCodingRes= {results : {geometry :{location :{ lat :number,lng : number}}}[]
+status :"OK" | "ZERO RESULTS"
 }
 
-form.addEventListener("submit",searchAddressHandler)
+function searchAddressHandler(event: Event) {
+  event.preventDefault()
+  const entredAddress = addressInput.value
+  axios
+    .get<GoogleGeoCodingRes>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        entredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+        if (response.data.status !== "OK") {
+            throw new Error("could not fetch location")
+        }
+      const coordinate = response.data.results[0].geometry.location
+    })
+    .catch((err) => console.log(err))
+}
+
+form.addEventListener("submit", searchAddressHandler)
