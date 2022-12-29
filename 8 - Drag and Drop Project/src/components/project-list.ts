8ccd004decf879338d85663d07f32e4c892511b1
Majdi Mokhtar <majdi.mokhtar@gmail.com>
2022-12-29 11:58:36 +0100

    typescript and webpack

--- 8 - Drag and Drop Project/src/components/project-list.ts
+++ 8 - Drag and Drop Project/src/components/project-list.ts
@@ -1,9 +1,9 @@
-import { projectState } from "../state/project-state.js"
-import { Component } from "./base-component.js"
-import { DragTarget } from "../models/drag-drop.js"
-import { autobind } from "../decorators/autobind.js"
-import { Project,ProjectStatus } from "../models/project.js"
-import { ProjectItem } from "./project-item.js"
+import { projectState } from "../state/project-state"
+import { Component } from "./base-component"
+import { DragTarget } from "../models/drag-drop"
+import { autobind } from "../decorators/autobind"
+import { Project,ProjectStatus } from "../models/project"
+import { ProjectItem } from "./project-item"
   // ProjectList Class
   export class ProjectList
     extends Component<HTMLDivElement, HTMLElement>
